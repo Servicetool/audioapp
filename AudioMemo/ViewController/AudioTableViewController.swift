c092bef3fa4aba1cc9b7c4281fdfdd5156f05982
Filip Nikolić <yutra@filips-macbook-pro.local>
2018-05-28 14:40:20 +0200

    first commit

---
+++ AudioMemo/ViewController/AudioTableViewController.swift
@@ -0,0 +1,30 @@
+//
+// AudioTableViewControllerswift
+//  AudioMemo
+//
+//  Created by Filip Nikolić on 23.05.18.
+//  Copyright © 2018 Filip Nikolić. All rights reserved.
+//
+
+import UIKit
+
+class AudioTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Entfernt Status Bar
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+
+}
+
---
+++ AudioMemo/ViewController/RecordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecordViewController.swift
+//  AudioMemo
+//
+//  Created by Filip Nikolić on 25.05.18.
+//  Copyright © 2018 Filip Nikolić. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
